92a8a15f0e28f9c9d7c6f4fd4e2702ce4d16d129
TuanLQ <tuanlqhe171554@fpt.edu.vn>
2024-06-14 00:57:13 +0700

    profile

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './pages/login/Login';
-import Register from './pages/register/Resgister';
-import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home></Home>
-    {/* <Login></Login> */}
-    {/* <Register></Register> */}
+    <Profile/>
   </React.StrictMode>
 );
