34c7d8d9702d3a45da76f20d086a98fbd4a71930
JerryShery <quanpmhe171342@fpt.edu.vn>
2024-06-14 00:16:46 +0700

    side bar and topbar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from './pages/login/Login';
 import Register from './pages/register/Resgister';
+import Home from './pages/home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
-     <Login/>
-    <Register/>
+    <Home></Home>
+    {/* <Login></Login> */}
+    <Register></Register>
   </React.StrictMode>
 );
